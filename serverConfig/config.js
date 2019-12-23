57ffc68fd2a65582442d4d99003d8b51127873e8
Jinx <649728226@qq.com>
2019-12-23 10:26:50 +0800

    Update config.js

--- serverConfig/config.js
+++ serverConfig/config.js
@@ -1,8 +1,8 @@
 let app = {
   user: 'sa',
-  password: '123456',
+  password: '',
   server: 'localhost',
-  database: 'dt_EnterpriseBenefit',
+  database: 'database',
   port: '',
   options: {
     encrypt: true // Use this if you're on Windows Azure
